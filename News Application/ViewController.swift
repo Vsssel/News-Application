433f2a495ed8e8ace2a856849febfd7734ce96b9
Vsssel <asselonay@gmail.com>
2024-11-21 14:58:52 +0500

    init

---
+++ News Application/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News Application
+//
+//  Created by Assel Artykbay on 21.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
